68775aa746c5789d7b13e4398509bf6c1d8aa6aa
rgrg2345 <s358tw@gmail.com>
2016-12-27 10:43:23 +0800

    update pwntool

--- cyfind.py
+++ cyfind.py
@@ -9,11 +9,13 @@ def h2ord(h):
    lens=len(h)/2
    s=""
    for i in range(lens):
-       s+=h[i*2:(i+1)*2].decode('hex')
-   return s
+       s=h[i*2:(i+1)*2]+s
+   return s.decode('hex')
+
 bofstr=cyclic(searchlen)
 dst=h2ord(argv[1])
-for i in range(searchlen-len(dst)):
+for i in range(0,searchlen,4):
+    #print "compare :%s with %s"%(bofstr[i:i+4],dst)
     if dst==bofstr[i:i+4]:
-        print argv[1],"found at offset:",i+3 if i>0 else 0
+        print argv[1],"found at offset:",i if i>0 else 0
         break
